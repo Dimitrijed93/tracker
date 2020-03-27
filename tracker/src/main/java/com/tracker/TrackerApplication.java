eeb65c274e50264fdd5cb4de457db0877f504963
Dimitrije Dragicevic <lich@localhost.localdomain>
2020-03-27 18:56:25 +0100

    fix jpa issues

--- tracker/src/main/java/com/tracker/TrackerApplication.java
+++ tracker/src/main/java/com/tracker/TrackerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 public class TrackerApplication {
 
 	public static void main(String[] args) {
